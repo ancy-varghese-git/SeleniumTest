94362c22f4bc1ce641ab15935af6ffa67a9ca210
= <=>
2020-09-03 21:46:14 +0530

    Changes to point to Selenium image and firefox browser

--- src/test/java/com/ann/casestudyone/BaseSeleniumTests.java
+++ src/test/java/com/ann/casestudyone/BaseSeleniumTests.java
@@ -8,52 +8,52 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.remote.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public abstract class BaseSeleniumTests {
-	private static final String CHROMEDRIVER_EXE = "chromedriver";
+	private static final String CHROMEDRIVER_EXE = "chromedriver.exe";
 
-	protected WebDriver driver;
+    protected WebDriver driver;
 
-	@Before
-	public void setUp() {
-		try {
-			// String driverFile = findFile();
+    @Before
+    public void setUp() {
+        /*String driverFile = findFile();
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(driverFile))
+                .build();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+        options.addArguments("--headless");
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.merge(capabilities);
+        this.driver = new ChromeDriver(service, options);*/
+    	
+    	try {
+    		this.driver = new RemoteWebDriver(new URL("http://40.113.221.175:4444/wd/hub"),new FirefoxOptions());
+    		}
+    		catch (MalformedURLException e) {
+    		    System.out.println("incorrect url");
+    		    }
+    }
 
-			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-			/*
-			 * ChromeDriverService service = new ChromeDriverService.Builder()
-			 * .usingDriverExecutable(new File(driverFile)) .build();
-			 */
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
-			options.addArguments("--headless");
-			options.setExperimentalOption("useAutomationExtension", false);
-			options.addArguments("start-maximized"); // open Browser in maximized mode
-			options.addArguments("disable-infobars"); // disabling infobars
-			options.addArguments("--disable-extensions"); // disabling extensions
-			options.addArguments("--disable-gpu"); // applicable to windows os only
-			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-			options.merge(capabilities);
+    private String findFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL url = classLoader.getResource(CHROMEDRIVER_EXE);
+        return url.getFile();
+    }
 
-			// this.driver = new ChromeDriver(service, options);
-			this.driver = new RemoteWebDriver(new URL("http://40.121.36.177:4444/wd/hub"), options);
-		} catch (MalformedURLException e) {
-			// if key is not a valid path then it cannot be resolved to an unmanged resource
-			System.out.println("incorrect url");
-		}
-	}
-
-	private String findFile() {
-		ClassLoader classLoader = getClass().getClassLoader();
-		URL url = classLoader.getResource(CHROMEDRIVER_EXE);
-		return url.getFile();
-	}
-
-	@After
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
--- src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
+++ src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
@@ -21,6 +21,17 @@ public class GoogleSearchPageTraditionalSeleniumTests extends BaseSeleniumTests 
         this.driver.get("https://www.google.com");
         WebElement element = this.driver.findElement(By.name("q"));
         assertNotNull(element);
+        
+        //we expect the title “Google “ should be present 
+		//String Expectedtitle = "Google";
+		//it will fetch the actual title 
+		//String Actualtitle = driver.getTitle();
+		//System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+		//it will compare actual title and expected title
+		//Assert.assertEquals(Actualtitle, Expectedtitle);
+		//print out the result
+		//System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+		 
     }
 
    /* @Test
