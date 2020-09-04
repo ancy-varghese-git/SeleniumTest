b383138d62e7712420c47d949e351467211c4de4
= <=>
2020-09-04 08:38:22 +0530

    Only firefox

--- src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
+++ src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
@@ -2,10 +2,16 @@ package com.ann.casestudyone;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,7 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GoogleSearchPageTraditionalSeleniumTests extends BaseSeleniumTests {
 
-	@Test
+	/*@Test
 	public void getSearchPage() {
 		this.driver.get("https://www.google.com");
 		WebElement element = this.driver.findElement(By.name("q"));
@@ -27,4 +33,14 @@ public class GoogleSearchPageTraditionalSeleniumTests extends BaseSeleniumTests 
 		assertEquals("Title mismatch", "Google", driver.getTitle());
 
 	}
+*/
+	@Test
+	public void getGoogleFirefox() {
+		try {
+			WebDriver driver = new RemoteWebDriver(new URL("http://40.113.221.175:4444/wd/hub"), new FirefoxOptions());
+			driver.get("http://www.google.com");
+		} catch (MalformedURLException e) {
+			System.out.println("incorrect url");
+		}
+	} 
 }
