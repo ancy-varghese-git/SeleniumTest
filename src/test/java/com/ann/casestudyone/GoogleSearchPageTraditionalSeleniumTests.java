5da15cb5bc108180b613dfb231949619c974156f
= <=>
2020-09-04 14:15:00 +0530

    More Selenium TestCases

--- src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
+++ src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
@@ -40,8 +40,16 @@ public class GoogleSearchPageTraditionalSeleniumTests {// extends BaseSeleniumTe
 		try {
 			WebDriver driver = new RemoteWebDriver(new URL("http://40.113.221.175:4444/wd/hub"), new FirefoxOptions());
 			driver.get("https://www.google.com");
-			System.out.println("this.driver.getTitle() " + driver.getTitle());
-			assertEquals("Google", driver.getTitle());
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.findElement(By.name("q")).sendKeys("Selenium");
+			String name = driver.findElement(By.name("q")).getAttribute("name");
+			
+			System.out.println("driver.findElement(By.name(\"q\")).getAttribute(\"name\") " + name);
+			assertNotNull(name);
+
+//			System.out.println("this.driver.getTitle() " + driver.getTitle());
+//			assertEquals("Google", driver.getTitle());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
