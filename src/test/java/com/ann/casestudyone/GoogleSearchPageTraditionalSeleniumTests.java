cff6b8b1d0d6674272c7fadff12765f03695a813
= <=>
2020-09-04 08:40:47 +0530

    Only firefox-without base

--- src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
+++ src/test/java/com/ann/casestudyone/GoogleSearchPageTraditionalSeleniumTests.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GoogleSearchPageTraditionalSeleniumTests extends BaseSeleniumTests {
+public class GoogleSearchPageTraditionalSeleniumTests {//extends BaseSeleniumTests {
 
 	/*@Test
 	public void getSearchPage() {
